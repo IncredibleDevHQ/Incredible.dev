5aac46de753c98d23216cf103f5c4050c5e9d690
Karthic Rao <kartronics85@gmail.com>
2024-02-28 00:08:41 -0800

    Adding boiler plate for fetch parent scope API
    - Establishing the route and doc for /parentscope api
    - Adding placeholder handler in controller
    - Creating type for request params

--- search/src/models.rs
+++ search/src/models.rs
@@ -29,3 +29,19 @@ pub struct SpanSearchRequest {
     // optional uid to track the request
     pub id: Option<String>,
 }
+
+
+/// Represents a request to fetch the parent scope of a specified code range within a file.
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct ParentScopeRequest {
+    /// The repository containing the target file.
+    pub repo: String,
+    /// The file path within the repository.
+    pub file: String,
+    /// The starting line number of the code range.
+    pub start_line: usize,
+    /// The ending line number of the code range.
+    pub end_line: usize,
+    /// An optional identifier for the request, which can be used for tracking or caching.
+    pub id: Option<String>,
+}
