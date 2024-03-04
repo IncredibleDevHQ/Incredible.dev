94f7b2caeb90c93ad05d3ba4f6750ee5871243fe
tiwarishankar <tiwarishankar587@gmail.com>
2024-03-04 12:39:59 +0530

    Add generate quickwit index name for local development Pass app state down the layer Remove hard coded value of quickwit db url

--- search/src/main.rs
+++ search/src/main.rs
@@ -1,10 +1,9 @@
 use anyhow::Context;
 use dotenv::dotenv;
+use log::{error, info};
+use std::env;
 use std::sync::Arc;
 use warp;
-use std::env;
-use log::{info, error};
-
 
 mod controller;
 mod db;
@@ -26,30 +25,32 @@ lazy_static! {
 }
 #[derive(Debug, Clone)]
 pub struct Configuration {
-    environment:String,
+    environment: String,
     symbol_collection_name: String,
     semantic_db_url: String,
     tokenizer_path: String,
     model_path: String,
     qdrant_api_key: Option<String>,
+    quikwit_db_url: String,
 }
 
 struct AppState {
     configuration: Configuration,
-    db_connection: db::DbConnect,  // Assuming DbConnection is your database connection type
+    db_connection: db::DbConnect, // Assuming DbConnection is your database connection type
 }
 
 async fn init_state() -> Result<AppState, anyhow::Error> {
     dotenv().ok();
     let configuration = Configuration {
         environment: env::var("ENVIRONMENT").context("ENVRINOMENT must be set")?,
-        symbol_collection_name: env::var("SYMBOL_COLLECTION_NAME").context("SYMBOL_COLLECTION_NAME must be set")?,
+        symbol_collection_name: env::var("SYMBOL_COLLECTION_NAME")
+            .context("SYMBOL_COLLECTION_NAME must be set")?,
         semantic_db_url: env::var("SEMANTIC_DB_URL").context("SEMANTIC_DB_URL must be set")?,
         tokenizer_path: env::var("TOKENIZER_PATH").unwrap_or("model/tokenizer.json".to_string()),
         model_path: env::var("MODEL_PATH").unwrap_or("model/model.onnx".to_string()),
+        quikwit_db_url: env::var("QUICKWIT_DB_URL").unwrap_or("quickwit_db_url".to_string()),
         qdrant_api_key: env::var("QDRANT_CLOUD_API_KEY").ok(),
     };
-
 
     let db_connection = db::init_db(configuration.clone()).await?;
 
