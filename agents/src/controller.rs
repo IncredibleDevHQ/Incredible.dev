0fe5aaa58be3e7f733fea329534e22d97919f77c
Karthic Rao <kartronics85@gmail.com>
2024-03-04 09:58:12 +0530

    Separating the initial query parsing logic
    - Cleaning the controller logic of the agent to separate the
      the initial query parsing logic.

--- agents/src/controller.rs
+++ agents/src/controller.rs
@@ -1,93 +1,73 @@
 use crate::agent;
-use crate::db_client;
+use crate::AppState;
 use agent::llm_gateway;
 use futures::StreamExt;
+use log::{error, info};
 use std::time::Duration;
-use log::{info, error};
-use crate::AppState;
 
 use crate::agent::agent::Action;
 use crate::agent::agent::Agent;
 use crate::agent::exchange::Exchange;
-use crate::config::Config;
-use crate::parser;
+use crate::parser::parser::{parse_query_target, parse_query};
 use crate::routes;
 use anyhow::Result;
-use core::panic;
 use std::convert::Infallible;
-use warp::http::StatusCode;
 use std::sync::Arc;
+use warp::http::StatusCode;
 
 pub async fn handle_retrieve_code(
     req: routes::RetrieveCodeRequest,
     app_state: Arc<AppState>,
 ) -> Result<impl warp::Reply, Infallible> {
     info!("Query: {}, Repo: {}", req.query, req.repo);
-    
+
     // Combine query and repo_name in the response
     let response = format!("Query: '{}', Repo: '{}'", req.query, req.repo);
 
-    let query = req.query;
+    // if query or repo is empty, return bad request.
+    if req.query.is_empty() || req.repo.is_empty() {
+        error!("Query or Repo from the user request is empty");
+        return Ok(warp::reply::with_status(
+            warp::reply::json(&format!("Error: Query or Repo is empty")),
+            StatusCode::BAD_REQUEST,
+        ));
+    }
 
-    let query_clone = query.clone();
+    // parse the query
+    let query_clone = req.query.clone();
+    let parsed_query = parse_query(req.query.clone());
+    // if the query is not parsed, return internal server error.
+    if parsed_query.is_err() {
+        error!("Error parsing the query");
+        return Ok(warp::reply::with_status(
+            warp::reply::json(&format!("Error: {}", parsed_query.err().unwrap())),
+            StatusCode::INTERNAL_SERVER_ERROR,
+        ));
+    }
 
-    let parse_query = match parser::parser::parse_nl(&query_clone) {
-        Ok(parsed) => {
-            // Adjust handling for `Option` type returned by `into_semantic`
-            match parsed.into_semantic() {
-                Some(semantic) => semantic.into_owned(),
-                None => {
-                    // Handle the case where `into_semantic` returns `None`
-                    error!("Error: got a 'Grep' query");
-                    // return error as API response 
-                    return Ok(warp::reply::with_status(
-                        warp::reply::json(&format!("Error: got a 'Grep' query")),
-                        StatusCode::BAD_REQUEST,
-                    ));
-                }
-            }
-        }
-        Err(_) => {
-            // Handle parse error, e.g., log it
-            eprintln!("Error: parse error");
-            // Since we can't return errors, consider logging or a default action
-            panic!("Error parsing query");
-        }
-    };
+    let parsed_query = parsed_query.unwrap();   
+    let query_target = parse_query_target(&parsed_query);
+    // if the query target is not parsed, return internal server error.
+    if query_target.is_err() {
+        error!("Error parsing the query target");
+        return Ok(warp::reply::with_status(
+            warp::reply::json(&format!("Error: {}", query_target.err().unwrap())),
+            StatusCode::INTERNAL_SERVER_ERROR,
+        ));
+    }
 
-    let query_target = match parse_query.target.as_ref() {
-        Some(target) => match target.as_plain() {
-            Some(plain) => plain.clone().into_owned(),
-            None => {
-                error!("Error: user query was not plain text");
-                // return error as API response
-                return Ok(warp::reply::with_status(
-                    warp::reply::json(&format!("Error: user query was not plain text")),
-                    StatusCode::BAD_REQUEST,
-                ));
-            }
-        },
-        None => {
-            error!("Error: query was empty");
-            // return error as API response
-            return Ok(warp::reply::with_status(
-                warp::reply::json(&format!("Error: query was empty")),
-                StatusCode::BAD_REQUEST,
-            ));
-        }
-    };
-
+    let query_target = query_target.unwrap();
     info!("Query target{:?}", query_target);
 
     let mut action = Action::Query(query_target);
     let id = uuid::Uuid::new_v4();
 
-    let mut exchanges = vec![agent::exchange::Exchange::new(id, parse_query.clone())];
-    exchanges.push(Exchange::new(id, parse_query));
+    let mut exchanges = vec![agent::exchange::Exchange::new(id, parsed_query.clone())];
+    exchanges.push(Exchange::new(id, parsed_query));
 
     // get the configuration from the app state
     let configuration = &app_state.configuration;
-    
+
     // intialize new llm gateway.
     let llm_gateway = llm_gateway::Client::new(&configuration.openai_url)
         .temperature(0.0)
