b466a3b823e3b70d0bf7824225bec9f987d9bba2
Subham Pramanik <subhampramanik@gmail.com>
2024-03-11 00:24:15 +0530

    Removed AppState in favour of lazy static config

--- modifier/src/main.rs
+++ modifier/src/main.rs
@@ -1,22 +1,16 @@
 use anyhow::Result;
-use log::{error, info};
-use reqwest::Client;
+use log::info;
+use once_cell::sync::Lazy;
 
 mod agent;
 mod controller;
+mod diff;
 mod models;
 mod routes;
 mod utils;
-mod diff;
 
 use core::result::Result::Ok;
-use std::sync::Arc;
-
-#[allow(unused)]
-struct AppState {
-    configuration: Configuration,
-    database_connection: DatabaseConnection,
-}
+use std::env;
 
 #[derive(Debug, Clone)]
 #[allow(unused)]
@@ -28,55 +22,37 @@ pub struct Configuration {
     openai_model: String,
 }
 
-#[derive(Debug, Clone)]
-#[allow(unused)]
-pub struct DatabaseConnection {
-    http_client: Client,
+impl Configuration {
+    fn load_from_env() -> Self {
+        let environment = std::env::var("RUST_ENV").unwrap_or_else(|_| "development".to_string());
+        let env_file = format!(".env.{}", environment);
+
+        info!("Loading configurations from {}", env_file);
+        dotenv::from_filename(env_file).ok();
+        Self {
+            environment: env::var("ENVIRONMENT").unwrap_or_else(|_| "development".to_string()),
+            code_search_url: env::var("CODE_SEARCH_URL")
+                .unwrap_or_else(|_| "http://127.0.0.1:3000".to_string()),
+            openai_url: env::var("OPENAI_URL")
+                .unwrap_or_else(|_| "https://api.openai.com".to_string()),
+            openai_api_key: env::var("OPENAI_API_KEY").unwrap_or_else(|_| {
+                "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".to_string()
+            }),
+            openai_model: env::var("OPENAI_MODEL")
+                .unwrap_or_else(|_| "gpt-4-1106-preview".to_string()),
+        }
+    }
 }
 
-async fn init_state() -> Result<AppState, anyhow::Error> {
-    let environment = std::env::var("RUST_ENV").unwrap_or_else(|_| "development".to_string());
-    let env_file = format!(".env.{}", environment);
-
-    info!("Loading configurations from {}", env_file);
-    dotenv::from_filename(env_file).ok();
-
-    let configuration = Configuration {
-        environment: std::env::var("ENVIRONMENT").unwrap_or("development".to_string()),
-        code_search_url: std::env::var("CODE_SEARCH_URL")
-            .unwrap_or("http://127.0.0.1:3000".to_string()),
-        openai_url: std::env::var("OPENAI_URL").unwrap_or("https://api.openai.com".to_string()),
-        openai_api_key: std::env::var("OPENAI_API_KEY")
-            .unwrap_or("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".to_string()),
-        openai_model: std::env::var("OPENAI_MODEL").unwrap_or("gpt-4-1106-preview".to_string()),
-    };
-    info!("Initialized configuration: {:?}", configuration);
-
-    let db_client = DatabaseConnection {
-        http_client: Client::new(),
-    };
-
-    Ok(AppState {
-        configuration,
-        database_connection: db_client,
-    })
-}
+static CONFIG: Lazy<Configuration> = Lazy::new(|| Configuration::load_from_env());
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    // Iniitialize app state and throw error if failed
-    let app_state = init_state().await;
-    let app_state = match app_state {
-        Ok(app_state) => Arc::new(app_state),
-        Err(err) => {
-            error!("Failed to initialize app state: {}", err);
-            std::process::exit(1);
-        }
-    };
+    info!("Loaded configuration: {:?}", *CONFIG);
 
-    let modification_routes = routes::modify_code(app_state);
+    let modification_routes = routes::modify_code();
     warp::serve(modification_routes)
         .run(([0, 0, 0, 0], 3001))
         .await;
