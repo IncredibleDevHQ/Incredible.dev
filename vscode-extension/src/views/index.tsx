39962895c352b6ca88099c14d43374790c9d5821
Subham Pramanik <subhampramanik@gmail.com>
2024-03-19 05:25:40 +0530

    Added panelState in context

--- vscode-extension/src/views/index.tsx
+++ vscode-extension/src/views/index.tsx
@@ -1,14 +1,31 @@
+import React, { ComponentType } from "react";
 import { createRoot } from "react-dom/client";
 import { WebviewApi, WithWebviewContext } from "./WebviewContext";
 import { ExampleViewA } from "./ExampleViewA";
 import { ExampleViewB } from "./ExampleViewB";
+import { ChatConversation } from "./ChatConversation";
+import { PanelState } from "../schema/PanelState";
+import { initalPanelState } from "../dummy";
 
 export const Views = {
   exampleViewA: ExampleViewA,
   exampleViewB: ExampleViewB,
+  chatConversation: ChatConversation,
 } as const;
 
 export type ViewKey = keyof typeof Views;
+
+interface ViewProps {
+  exampleViewA?: {};
+  exampleViewB?: {};
+  chatConversation?: { panelState: PanelState };
+}
+
+const initialProps: ViewProps = {
+  chatConversation: {
+    panelState: initalPanelState,
+  },
+};
 
 export function render<V extends ViewKey>(
   key: V,
@@ -23,13 +40,13 @@ export function render<V extends ViewKey>(
 
   __webpack_public_path__ = publicPath;
 
-  const Component: React.ComponentType = Views[key];
+  const Component: ComponentType<any> = Views[key];
+  const props = initialProps[key] || {};
 
-  const root = createRoot(container);
-
+  const root = createRoot(container!);
   root.render(
     <WithWebviewContext vscodeApi={vscodeApi}>
-      <Component />
+      <Component {...props} />
     </WithWebviewContext>
   );
 }
