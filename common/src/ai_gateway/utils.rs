284ebc50a28df836af9f4b66d12fb20120b0eeea
Karthic Rao <kartronics85@gmail.com>
2024-04-05 16:23:28 +0530

    Adding custom tokenizer.

---
+++ common/src/ai_gateway/utils.rs
@@ -0,0 +1,17 @@
+use lazy_static::lazy_static;
+use std::sync::{Arc, Mutex};
+
+/// Count how many tokens a piece of text needs to consume
+pub fn count_tokens(text: &str) -> usize {
+    cl100k_base_singleton()
+        .lock()
+        .encode_with_special_tokens(text)
+        .len()
+}
+
+pub fn cl100k_base_singleton() -> Arc<Mutex<CoreBPE>> {
+    lazy_static! {
+        static ref CL100K_BASE: Arc<Mutex<CoreBPE>> = Arc::new(Mutex::new(cl100k_base().unwrap()));
+    }
+    CL100K_BASE.clone()
+}
