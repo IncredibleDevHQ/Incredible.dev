70c3445f7774725ea5660529a2e905740c78af5e
Karthic Rao <kartronics85@gmail.com>
2024-04-28 06:51:22 -0700

    cleaning up the coordinator's env.

--- coordinator/src/configuration.rs
+++ coordinator/src/configuration.rs
@@ -3,15 +3,9 @@ use crate::CONFIG;
 #[allow(unused)]
 #[derive(Debug, Clone)]
 pub struct Configuration {
-    pub environment: String,
     pub code_search_url: String,
-    pub context_generator_url: String,
     pub code_understanding_url: String,
-    pub code_modifier_url: String,
     pub redis_url: String,
-    pub openai_url: String,
-    pub openai_api_key: String,
-    pub openai_model: String,
     pub ai_gateway_config: String,
 }
 
@@ -23,31 +17,11 @@ pub fn get_code_search_url() -> String {
     CONFIG.read().unwrap().code_search_url.clone()
 }
 
-pub fn get_context_generator_url() -> String {
-    CONFIG.read().unwrap().context_generator_url.clone()
-}
-
 pub fn get_code_understanding_url() -> String {
     CONFIG.read().unwrap().code_understanding_url.clone()
-}
-
-pub fn get_code_modifier_url() -> String {
-    CONFIG.read().unwrap().code_modifier_url.clone()
 }
 
 pub fn get_ai_gateway_config() -> String {
     log::debug!("Reading AI Gateway config");
     CONFIG.read().unwrap().ai_gateway_config.clone()
-}
-
-pub fn get_openai_url() -> String {
-    CONFIG.read().unwrap().openai_url.clone()
-}
-
-pub fn get_openai_api_key() -> String {
-    CONFIG.read().unwrap().openai_api_key.clone()
-}
-
-pub fn get_openai_model() -> String {
-    CONFIG.read().unwrap().openai_model.clone()
 }
--- coordinator/src/llm_ops/summarize.rs
+++ coordinator/src/llm_ops/summarize.rs
@@ -8,7 +8,7 @@ use common::prompts::{
     create_task_answer_summarization_prompt, generate_single_task_summarization_prompt,
 };
 
-use crate::configuration::{get_ai_gateway_config, get_openai_api_key, get_openai_model, get_openai_url};
+use crate::configuration::get_ai_gateway_config;
 
 pub async fn generate_summarized_answer_for_task(
     user_query: String,
@@ -58,58 +58,58 @@ pub async fn generate_summarized_answer_for_task(
     Ok(response_message)
 }
 
-pub async fn generate_single_task_summarization_(
-    user_query: &str,
-    search_url: &str,
-    task: &TaskDetailsWithContext,
-) -> Result<String, anyhow::Error> {
-    let open_ai_url = get_openai_url();
-    let open_ai_model = get_openai_model();
-    let open_ai_key = get_openai_api_key();
-    // Construct the summarization prompt for the given task and user query.
-    let summarization_prompt =
-        generate_single_task_summarization_prompt(user_query, search_url, task).await?;
+// pub async fn generate_single_task_summarization_(
+//     user_query: &str,
+//     search_url: &str,
+//     task: &TaskDetailsWithContext,
+// ) -> Result<String, anyhow::Error> {
+//     let open_ai_url = get_openai_url();
+//     let open_ai_model = get_openai_model();
+//     let open_ai_key = get_openai_api_key();
+//     // Construct the summarization prompt for the given task and user query.
+//     let summarization_prompt =
+//         generate_single_task_summarization_prompt(user_query, search_url, task).await?;
 
-    //debug!("Summarization prompt: {}", summarization_prompt);
+//     //debug!("Summarization prompt: {}", summarization_prompt);
 
-    let llm_gateway = llm_gateway::Client::new(&open_ai_url)
-        .temperature(0.0)
-        .bearer(open_ai_key)
-        .model(&open_ai_model);
-    let system_message = message::Message::system(&summarization_prompt);
-    // append the system message to the message history
-    let messages = Some(system_message.clone()).into_iter().collect::<Vec<_>>();
+//     let llm_gateway = llm_gateway::Client::new(&open_ai_url)
+//         .temperature(0.0)
+//         .bearer(open_ai_key)
+//         .model(&open_ai_model);
+//     let system_message = message::Message::system(&summarization_prompt);
+//     // append the system message to the message history
+//     let messages = Some(system_message.clone()).into_iter().collect::<Vec<_>>();
 
-    let response = match llm_gateway
-        .clone()
-        .model(&open_ai_model)
-        .chat(&messages, None)
-        .await
-    {
-        Ok(response) => Ok(response),
-        Err(e) => {
-            log::error!("Error: Unable to fetch response from the gateway: {:?}", e);
-            Err(anyhow::anyhow!("Unable to fetch response from the gateway"))
-        }
-    };
-    let final_response = match response {
-        Ok(response) => response,
-        Err(e) => {
-            log::error!("Error: Unable to fetch response from the gateway: {:?}", e);
-            // Return error as API response
-            return Err(anyhow::anyhow!(
-                "Unable to fetch response from the gateway: {:?}",
-                e
-            ));
-        }
-    };
+//     let response = match llm_gateway
+//         .clone()
+//         .model(&open_ai_model)
+//         .chat(&messages, None)
+//         .await
+//     {
+//         Ok(response) => Ok(response),
+//         Err(e) => {
+//             log::error!("Error: Unable to fetch response from the gateway: {:?}", e);
+//             Err(anyhow::anyhow!("Unable to fetch response from the gateway"))
+//         }
+//     };
+//     let final_response = match response {
+//         Ok(response) => response,
+//         Err(e) => {
+//             log::error!("Error: Unable to fetch response from the gateway: {:?}", e);
+//             // Return error as API response
+//             return Err(anyhow::anyhow!(
+//                 "Unable to fetch response from the gateway: {:?}",
+//                 e
+//             ));
+//         }
+//     };
 
-    let choices_str = final_response.choices[0]
-        .message
-        .content
-        .clone()
-        .unwrap_or_else(|| "".to_string());
+//     let choices_str = final_response.choices[0]
+//         .message
+//         .content
+//         .clone()
+//         .unwrap_or_else(|| "".to_string());
 
-    debug!("Summarized answer: {}", choices_str);
-    Ok(choices_str)
-}
+//     debug!("Summarized answer: {}", choices_str);
+//     Ok(choices_str)
+// }
