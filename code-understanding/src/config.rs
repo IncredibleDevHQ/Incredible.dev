267b0fe3914897e8d14b3e04dba4c78f2cddcd0f
Karthic Rao <kartronics85@gmail.com>
2024-04-28 05:50:11 -0700

    Cleaning up env configurations for code understanding service.

--- code-understanding/src/config.rs
+++ code-understanding/src/config.rs
@@ -1,16 +1,11 @@
-use std::{env, fs};
 use log::info;
+use std::{env, fs};
 
 use crate::CONFIG;
 #[derive(Clone, Debug)]
 pub struct Config {
-    pub semantic_url: String,
     pub qdrant_api_key: Option<String>,
-    pub tokenizer_path: String,
-    pub model_path: String,
-    pub openai_key: String,
-    pub openai_url: String,
-    pub openai_model: String,
+    pub semantic_url: String,
     pub quickwit_url: String,
     pub semantic_collection_name: String,
     pub search_server_url: String,
@@ -38,20 +33,10 @@ pub fn load_from_env() -> Config {
 
     info!("Env configuration along with AI Gateway configuration loaded successfully.");
 
-    // Directly use `?` to propagate the error if the environment variable is not set for OPENAI_KEY
-    let openai_key =
-        std::env::var("OPENAI_KEY").expect("OPENAI_KEY environment variable is not set");
-
+    let qdrant_api_key = env::var("QDRANT_API_KEY").ok();
     // Default values are provided using `unwrap_or_else` for other variables
     let semantic_url =
         env::var("SEMANTIC_URL").unwrap_or_else(|_| "http://localhost:6334".to_string());
-    let qdrant_api_key = env::var("QDRANT_API_KEY").ok(); // It's optional
-    let tokenizer_path =
-        env::var("TOKENIZER_PATH").unwrap_or_else(|_| "./model/tokenizer.json".to_string());
-    let model_path = env::var("MODEL_PATH").unwrap_or_else(|_| "./model/model.onnx".to_string());
-    let openai_url =
-        env::var("OPENAI_URL").unwrap_or_else(|_| "https://api.openai.com".to_string());
-    let openai_model = env::var("OPENAI_MODEL").unwrap_or_else(|_| "gpt-4".to_string());
     let quickwit_url =
         env::var("QUICKWIT_URL").unwrap_or_else(|_| "http://localhost:7280".to_string());
     let semantic_collection_name =
@@ -60,14 +45,9 @@ pub fn load_from_env() -> Config {
         env::var("SEARCH_SERVER_URL").unwrap_or_else(|_| "http://localhost:3003".to_string());
     let redis_url = env::var("REDIS_URL").unwrap_or_else(|_| "redis://localhost:6379".to_string());
 
-   Config {
-        semantic_url,
+    Config {
         qdrant_api_key,
-        tokenizer_path,
-        model_path,
-        openai_key,
-        openai_url,
-        openai_model,
+        semantic_url,
         quickwit_url,
         semantic_collection_name,
         search_server_url,
@@ -78,30 +58,6 @@ pub fn load_from_env() -> Config {
 
 pub fn get_semantic_url() -> String {
     CONFIG.read().unwrap().semantic_url.clone()
-}
-
-pub fn get_qdrant_api_key() -> Option<String> {
-    CONFIG.read().unwrap().qdrant_api_key.clone()
-}
-
-pub fn get_tokenizer_path() -> String {
-    CONFIG.read().unwrap().tokenizer_path.clone()
-}
-
-pub fn get_model_path() -> String {
-    CONFIG.read().unwrap().model_path.clone()
-}
-
-pub fn get_openai_key() -> String {
-    CONFIG.read().unwrap().openai_key.clone()
-}
-
-pub fn get_openai_url() -> String {
-    CONFIG.read().unwrap().openai_url.clone()
-}
-
-pub fn get_openai_model() -> String {
-    CONFIG.read().unwrap().openai_model.clone()
 }
 
 pub fn get_quickwit_url() -> String {
